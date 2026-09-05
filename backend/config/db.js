e6dddd24cc8fd1a2a89e656109c3965984113b3b
Mokbil3 <91195080+Mokbil3@users.noreply.github.com>
2026-09-05 23:25:03 +0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,8 +10,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 20,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
 export default pool;
