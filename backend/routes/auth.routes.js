c8384c77e5e7fe3e5131f665cf42fcee438763e1
Mokbil3 <91195080+Mokbil3@users.noreply.github.com>
2026-09-06 03:26:50 +0400

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,17 +1,70 @@
 import express from "express";
+import bcrypt from "bcryptjs";
+import db from "../config/db.js";
 
 const router = express.Router();
 
 /*
-  Test Route
-  GET /api/auth/test
+   TEST ROUTE
 */
-
 router.get("/test", (req, res) => {
   res.json({
     success: true,
     message: "Auth Route Working"
   });
+});
+
+/*
+   REGISTER
+   POST /api/auth/register
+*/
+router.post("/register", async (req, res) => {
+  try {
+    const { full_name, email, password } = req.body;
+
+    if (!full_name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      });
+    }
+
+    const [existingUser] = await db.query(
+      "SELECT id FROM users WHERE email = ?",
+      [email]
+    );
+
+    if (existingUser.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.query(
+      `
+      INSERT INTO users
+      (full_name, email, password)
+      VALUES (?, ?, ?)
+      `,
+      [full_name, email, hashedPassword]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully"
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
 });
 
 export default router;
