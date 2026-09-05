8951c8782c0fb3e72b158ff3c9f5e621b5a0fab0
Mokbil3 <91195080+Mokbil3@users.noreply.github.com>
2026-09-06 03:25:45 +0400

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
+/*
+  Test Route
+  GET /api/auth/test
+*/
+
 router.get("/test", (req, res) => {
   res.json({
     success: true,
